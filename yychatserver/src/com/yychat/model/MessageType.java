c80f75ef8a0f2e05dc23e8d0d0138929e8c1e217
JooyLeo <2213765792@qq.com>
2019-04-21 01:29:26 +0800

    first commit

---
+++ yychatserver/src/com/yychat/model/MessageType.java
@@ -0,0 +1,11 @@
+package com.yychat.model;
+
+public interface MessageType {	
+    String message_LoginSuccess="1";//×Ö·û´®³£Á¿
+	String message_LoginFailure="0";
+	String message_Common="2";
+	String message_RequestOlineFriend="3";
+	String message_OnlineFriend="4";
+	
+	
+}
